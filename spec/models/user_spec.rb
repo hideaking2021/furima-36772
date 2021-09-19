e0e0a34a647d0fd4efaa4a6ac69933a0c89cda22
hideaking2021 <hide.freetimes.2021@gmail.com>
2021-09-19 14:35:19 +0900

    修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,6 +60,17 @@ RSpec.describe User, type: :model do
         @user.password_confirmation = 'a23456'
         expect(@user).to be_valid
       end
+      it 'passwordが6文字以上であれば登録できる' do
+        @user.password = 'a23456'
+        @user.password_confirmation = 'a23456'
+        expect(@user).to be_valid
+      end
+      it '全角文字を含むパスワードでは登録できない' do
+        @user.password = 'テストa2345'
+        @user.password_confirmation = 'テストa2345'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is invalid')
+      end
       
       
       it 'first_nameが空では登録できない' do
@@ -95,6 +106,11 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('Last name kana is invalid')
       end
       it 'last_name_kanaが半角英数字では登録できない' do
+        @user.last_name_kana = 'yamada123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name kana is invalid')
+      end
+      it 'last_name_kanaが半角文字含まれていると登録できない' do
         @user.last_name_kana = 'yamada'
         @user.valid?
         expect(@user.errors.full_messages).to include('Last name kana is invalid')
@@ -110,6 +126,11 @@ RSpec.describe User, type: :model do
         expect(@user.errors.full_messages).to include('First name kana is invalid')
       end
       it 'first_name_kanaが半角英数字では登録できない' do
+        @user.first_name_kana = 'taro123'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name kana is invalid')
+      end
+      it 'first_name_kanaに半角文字が含まれていると登録できない' do
         @user.first_name_kana = 'taro'
         @user.valid?
         expect(@user.errors.full_messages).to include('First name kana is invalid')
