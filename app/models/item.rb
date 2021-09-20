69382bd9ec7ec5d0ec9317b3f347f5397caee8b5
hideaking2021 <hide.freetimes.2021@gmail.com>
2021-09-20 15:51:40 +0900

    Active Storage・ActiveHash実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  # Association
+    belongs_to :user
+    has_one_attached :image
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :fee
+    belongs_to :area
+    belongs_to :shopping_day
+
+  # validation
+  with_options presence: true do
+    validates :name
+    validates :description
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :fee_id
+    validates :area_id
+    validates :shopping_day_id
+  end
+
 end
