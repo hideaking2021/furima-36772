5b17a819461c2c394bff606534ca3a4202a3c949
hideaking2021 <hide.freetimes.2021@gmail.com>
2021-09-23 12:24:34 +0900

    重複修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
-  
+  before_action :move_to_index, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -16,7 +16,7 @@ class ItemsController < ApplicationController
   end
   
   def edit
-    redirect_to root_path if current_user.id != @item.user_id
+    
   end
 
   def create
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path
+    
   end
 
 
@@ -46,6 +46,10 @@ class ItemsController < ApplicationController
 
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :fee_id, :area_id, :shopping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id != @item.user_id
   end
 
   def set_item
