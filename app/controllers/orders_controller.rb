7f4e4a642d4e08451e356003553892f3efd4bffd
hideaking2021 <hide.freetimes.2021@gmail.com>
2021-09-26 17:34:53 +0900

    商品購入ページへ遷移しようとすると、トップページに遷移

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item
-  
+  before_action :move_to_index, only:[:index, :create]
 
   def index
     @order_delivery = OrderDelivery.new
@@ -27,6 +27,12 @@ class OrdersController < ApplicationController
 
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    redirect_to root_path if current_user.id == @item.user_id 
+
+    redirect_to root_path if @item.order
   end
 
   def pay_item
